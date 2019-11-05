033c00991db24b5139e25726ecb6cff821be68b2
josealfon <josealf.perez@gmail.com>
2019-11-05 20:38:36 +0100

    Añado dataframe

--- iris_eda_app.py
+++ iris_eda_app.py
@@ -1,10 +1,16 @@
 import streamlit as st 
+import pandas as pd
 
 def main():
 	""" A simple Iris EDA App """
 
 	st.title("Iris EDA App with streamlit")
 	st.subheader("Streamlit is Cool")
+	df1 = pd.DataFrame({
+  		'first column': [1, 2, 3, 4],
+  		'second column': [10, 20, 30, 40]
+})
+	st.dataframe(df1)
 
 if __name__ == '__main__':
 	main()
